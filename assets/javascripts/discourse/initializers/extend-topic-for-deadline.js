16ce892c8ac3cc8b6a142c42257f0d6328890cd4
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2025-11-14 13:27:50 +0100

    chore: testing api initializer for plugin outlet

--- assets/javascripts/discourse/initializers/extend-topic-for-deadline.js
+++ assets/javascripts/discourse/initializers/extend-topic-for-deadline.js
@@ -1,9 +1,23 @@
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("1.34", (api) => {
-	api.registerValueTransformer("topic-list-columns", ({ value: columns }) => {
-		console.log("[deadline-plugin] Transformer registered");
+	api.renderInOutlet("after-topic-list-item", (outletArgs) => {
+		const topic = outletArgs.topic;
 
-		return columns;
+		const category = topic.category_id;
+		const closed = topic.closed;
+		const solved = topic.has_accepted_answer === true;
+		const categoryIncluded =
+			siteSettings.deadlineAllowedCategories?.includes(category) ?? true;
+
+		console.log({ topic });
+		console.log({ category, closed, solved, categoryIncluded });
+
+		if (!categoryIncluded) return;
+		if (!siteSettings.deadlineDisplayOnClosedTopic && closed) return;
+		if (!siteSettings.deadlineDisplayOnSolvedTopic && solved) return;
+		if (!topic.deadline_timestamp) return;
+
+		return null;
 	});
 });
