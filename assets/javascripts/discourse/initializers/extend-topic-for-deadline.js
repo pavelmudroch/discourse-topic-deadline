ac55e4715efda4c3304c3cf2f9de3c03728de678
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2025-11-14 14:21:39 +0100

    fix: other undefines

--- assets/javascripts/discourse/initializers/extend-topic-for-deadline.js
+++ assets/javascripts/discourse/initializers/extend-topic-for-deadline.js
@@ -1,19 +1,24 @@
 import { apiInitializer } from "discourse/lib/api";
 import { withPluginApi } from "discourse/lib/plugin-api";
+import { getSiteSettings } from "../../lib/get-site-settings";
 import { TopicDeadline } from "../components/topic-deadline";
 
 console.log("Initializing extend-topic-for-deadline...");
 export default apiInitializer("1.34", (api) => {
-	console.log("Rendering TopicDeadline component...");
-	api.renderInOutlet("after-topic-list-item", TopicDeadline);
-	console.log(TopicDeadline);
+	try {
+		console.log("Rendering TopicDeadline component...");
+		api.renderInOutlet("after-topic-list-item", TopicDeadline);
+		console.log(TopicDeadline);
 
-	const siteSettings = withPluginApi("1.34", (api) => getSiteSettings(api));
-	if (!siteSettings.deadlineEnabled) {
-		console.log("Deadline plugin is disabled.");
-		return;
+		const siteSettings = withPluginApi("1.34", (api) => getSiteSettings(api));
+		if (!siteSettings.deadlineEnabled) {
+			console.log("Deadline plugin is disabled.");
+			return;
+		}
+		console.log(siteSettings);
+	} catch (error) {
+		console.error(error);
 	}
-	console.log(siteSettings);
 
 	// api.renderInOutlet("after-topic-list-item", (outletArgs) => {
 	// 	const topic = outletArgs.topic;
