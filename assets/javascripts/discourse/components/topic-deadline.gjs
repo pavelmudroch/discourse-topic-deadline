8ae9a07f25b7500a1127ec9eb6618a3a76c67b68
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2025-11-14 14:37:25 +0100

    chore: debug logs

--- assets/javascripts/discourse/components/topic-deadline.gjs
+++ assets/javascripts/discourse/components/topic-deadline.gjs
@@ -1,7 +1,51 @@
 import Component from "@glimmer/component";
 
 export class TopicDeadline extends Component {
+    get topic() {
+        return this.args.topic;
+    }
+
+    get deadlineDisplayOnClosedTopic() {
+        return this.args.deadlineDisplayOnClosedTopic;
+    }
+
+    get deadlineDisplayOnSolvedTopic() {
+        return this.args.deadlineDisplayOnSolvedTopic;
+    }
+
+    get deadlineAllowedCategories() {
+        return this.args.deadlineAllowedCategories;
+    }
+
+    get shouldRender() {
+        const category = this.topic.category_id;
+        const closed = this.topic.closed;
+        const solved = this.topic.has_accepted_answer === true;
+        const categoryIncluded =
+            this.deadlineAllowedCategories?.includes(category) ?? true;
+
+        console.log({
+            category,
+            closed,
+            solved,
+            categoryIncluded,
+            deadlineDisplayOnClosedTopic: this.deadlineDisplayOnClosedTopic,
+            deadlineDisplayOnSolvedTopic: this.deadlineDisplayOnSolvedTopic,
+            deadline_timestamp: this.topic.deadline_timestamp,
+            deadlineAllowedCategories: this.deadlineAllowedCategories,
+        });
+
+        if (!categoryIncluded) return false;
+        if (!this.deadlineDisplayOnClosedTopic && closed) return false;
+        if (!this.deadlineDisplayOnSolvedTopic && solved) return false;
+        if (!this.topic.deadline_timestamp) return false;
+
+        return true;
+    }
+
     <template>
-        <div>XXX</div>
+        {{#if this.shouldRender}}
+            XXX
+        {{/if}}
     </template>
 }
--- assets/javascripts/discourse/initializers/extend-topic-for-deadline.js
+++ assets/javascripts/discourse/initializers/extend-topic-for-deadline.js
@@ -3,40 +3,22 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 import { getSiteSettings } from "../../lib/get-site-settings";
 import { TopicDeadline } from "../components/topic-deadline";
 
-console.log("Initializing extend-topic-for-deadline...");
 export default apiInitializer("1.34", (api) => {
 	try {
-		console.log("Rendering TopicDeadline component...");
-		api.renderInOutlet("after-topic-list-item", TopicDeadline);
-		console.log(TopicDeadline);
-
 		const siteSettings = withPluginApi("1.34", (api) => getSiteSettings(api));
 		if (!siteSettings.deadlineEnabled) {
 			console.log("Deadline plugin is disabled.");
 			return;
 		}
-		console.log(siteSettings);
+
+		api.renderInOutlet("after-topic-list-item", TopicDeadline, {
+			params: {
+				deadlineDisplayOnClosedTopic: siteSettings.deadlineDisplayOnClosedTopic,
+				deadlineDisplayOnSolvedTopic: siteSettings.deadlineDisplayOnSolvedTopic,
+				deadlineAllowedCategories: siteSettings.deadlineAllowedCategories,
+			},
+		});
 	} catch (error) {
 		console.error(error);
 	}
-
-	// api.renderInOutlet("after-topic-list-item", (outletArgs) => {
-	// 	const topic = outletArgs.topic;
-
-	// 	const category = topic.category_id;
-	// 	const closed = topic.closed;
-	// 	const solved = topic.has_accepted_answer === true;
-	// 	const categoryIncluded =
-	// 		siteSettings.deadlineAllowedCategories?.includes(category) ?? true;
-
-	// 	console.log({ topic });
-	// 	console.log({ category, closed, solved, categoryIncluded });
-
-	// 	if (!categoryIncluded) return;
-	// 	if (!siteSettings.deadlineDisplayOnClosedTopic && closed) return;
-	// 	if (!siteSettings.deadlineDisplayOnSolvedTopic && solved) return;
-	// 	if (!topic.deadline_timestamp) return;
-
-	// 	return null;
-	// });
 });
