8bd570a8ab72c876dd478e0915485c9cb564059c
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2026-03-19 12:44:00 +0100

    fix: replace deprecated htmlSafe for trustHTML in handlebar

--- assets/javascripts/discourse/connectors/topic-above-posts/topic-deadline.js
+++ assets/javascripts/discourse/connectors/topic-above-posts/topic-deadline.js
@@ -1,168 +1,167 @@
-import { getOwner } from '@ember/application';
-import { action } from '@ember/object';
-import { service } from '@ember/service';
-import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import I18n from 'discourse-i18n';
-import { ajax } from 'discourse/lib/ajax';
-import { getDeadlineAllowedCategories } from '../../../lib/get-deadline-allowed-categories';
-import { getDeadlineRemainingDays } from '../../../lib/get-deadline-remaining-days';
-import { getDeadlineRemainingDaysClass } from '../../../lib/get-deadline-remaining-days-class';
-import { translateDeadlineRemainingDays } from '../../../lib/translate-deadline-remaining-days';
-import DeadlineCalendar from '../../components/modal/deadline-calendar';
+import { getOwner } from "@ember/application";
+import { action } from "@ember/object";
+import { service } from "@ember/service";
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import I18n from "discourse-i18n";
+import { ajax } from "discourse/lib/ajax";
+import { getDeadlineAllowedCategories } from "../../../lib/get-deadline-allowed-categories";
+import { getDeadlineRemainingDays } from "../../../lib/get-deadline-remaining-days";
+import { getDeadlineRemainingDaysClass } from "../../../lib/get-deadline-remaining-days-class";
+import { translateDeadlineRemainingDays } from "../../../lib/translate-deadline-remaining-days";
+import DeadlineCalendar from "../../components/modal/deadline-calendar";
 
 function getDeadlineColorsFromClassName(className) {
-    const temp = document.createElement('span');
-    temp.style.visibility = 'hidden';
-    temp.classList.add('topic-deadline-date', className);
-    document.body.appendChild(temp);
-    const style = window.getComputedStyle(temp);
-    const colorStyle = style.color;
-    const backgroundColorStyle = style.backgroundColor;
-    temp.remove();
+	const temp = document.createElement("span");
+	temp.style.visibility = "hidden";
+	temp.classList.add("topic-deadline-date", className);
+	document.body.appendChild(temp);
+	const style = window.getComputedStyle(temp);
+	const colorStyle = style.color;
+	const backgroundColorStyle = style.backgroundColor;
+	temp.remove();
 
-    const color =
-        colorStyle === 'rgba(0, 0, 0)' ? null : `color:${colorStyle};`;
-    const backgroundColor =
-        backgroundColorStyle === 'rgba(0, 0, 0, 0)'
-            ? null
-            : `background-color:${backgroundColorStyle};`;
+	const color = colorStyle === "rgba(0, 0, 0)" ? null : `color:${colorStyle};`;
+	const backgroundColor =
+		backgroundColorStyle === "rgba(0, 0, 0, 0)"
+			? null
+			: `background-color:${backgroundColorStyle};`;
 
-    return { color, backgroundColor };
+	return { color, backgroundColor };
 }
 
 export default class TopicDeadline extends Component {
-    @service siteSettings;
-    @service appEvents;
-    @tracked deadlineFormatted;
-    @tracked hasDeadline;
-    @tracked style;
-    @tracked shouldRender;
-    #topic;
-    #timestamp;
-    #remainingDays;
+	@service siteSettings;
+	@service appEvents;
+	@tracked deadlineFormatted;
+	@tracked hasDeadline;
+	@tracked style;
+	@tracked shouldRender;
+	#topic;
+	#timestamp;
+	#remainingDays;
 
-    get id() {
-        return this.#topic.id;
-    }
+	get id() {
+		return this.#topic.id;
+	}
 
-    get deadlineTimestamp() {
-        return this.#timestamp;
-    }
+	get deadlineTimestamp() {
+		return this.#timestamp;
+	}
 
-    get #deadlineFormatted() {
-        const timestamp = this.deadlineTimestamp;
+	get #deadlineFormatted() {
+		const timestamp = this.deadlineTimestamp;
 
-        if (timestamp === null || timestamp === '')
-            return I18n.t('deadline.change_button.error');
+		if (timestamp === null || timestamp === "")
+			return I18n.t("deadline.change_button.error");
 
-        const timestampFormatted = new Date(timestamp).toLocaleDateString(
-            I18n.t('deadline.date_locales'),
-            {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-            },
-        );
-        const deadlineDayFormatted = translateDeadlineRemainingDays(
-            this.#remainingDays,
-        );
-        const deadlineFormatted = `${
-            deadlineDayFormatted?.concat(' - ') ?? ''
-        }${timestampFormatted}`;
+		const timestampFormatted = new Date(timestamp).toLocaleDateString(
+			I18n.t("deadline.date_locales"),
+			{
+				weekday: "long",
+				year: "numeric",
+				month: "long",
+				day: "numeric",
+			},
+		);
+		const deadlineDayFormatted = translateDeadlineRemainingDays(
+			this.#remainingDays,
+		);
+		const deadlineFormatted = `${
+			deadlineDayFormatted?.concat(" - ") ?? ""
+		}${timestampFormatted}`;
 
-        return deadlineFormatted;
-    }
+		return deadlineFormatted;
+	}
 
-    get #hasDeadline() {
-        return this.deadlineTimestamp !== null;
-    }
+	get #hasDeadline() {
+		return this.deadlineTimestamp !== null;
+	}
 
-    constructor() {
-        super(...arguments);
+	constructor() {
+		super(...arguments);
 
-        this.appEvents.on('page:changed', this, this.#onPageChanged);
-        this.appEvents.on('deadline:changed', this, this.#update);
-        this.#init(this.args.outletArgs.model);
-        this.updateDeadline();
-    }
+		this.appEvents.on("page:changed", this, this.#onPageChanged);
+		this.appEvents.on("deadline:changed", this, this.#update);
+		this.#init(this.args.outletArgs.model);
+		this.updateDeadline();
+	}
 
-    @action
-    setDeadline() {
-        const modal = getOwner(this).lookup('service:modal');
-        modal.show(DeadlineCalendar, {
-            model: this.args.outletArgs.model,
-        });
-    }
+	@action
+	setDeadline() {
+		const modal = getOwner(this).lookup("service:modal");
+		modal.show(DeadlineCalendar, {
+			model: this.args.outletArgs.model,
+		});
+	}
 
-    updateDeadline() {
-        this.shouldRender = this.#shouldRender();
-        this.deadlineFormatted = this.#deadlineFormatted;
-        this.hasDeadline = this.#hasDeadline;
-        const className = getDeadlineRemainingDaysClass(
-            this.#remainingDays,
-            this.siteSettings.deadline_soon_days_threshold,
-        );
-        const { color, backgroundColor } =
-            getDeadlineColorsFromClassName(className);
-        this.style = [color ?? '', backgroundColor ?? ''].join('');
-    }
+	updateDeadline() {
+		this.shouldRender = this.#shouldRender();
+		this.deadlineFormatted = this.#deadlineFormatted;
+		this.hasDeadline = this.#hasDeadline;
+		const className = getDeadlineRemainingDaysClass(
+			this.#remainingDays,
+			this.siteSettings.deadline_soon_days_threshold,
+		);
+		const { color, backgroundColor } =
+			getDeadlineColorsFromClassName(className);
+		this.style = [color ?? "", backgroundColor ?? ""].join("");
+	}
 
-    willDestroy() {
-        this.appEvents.off('deadline:changed', this, this.#update);
-        this.appEvents.off('page:changed', this, this.#onPageChanged);
-        super.willDestroy(...arguments);
-    }
+	willDestroy() {
+		this.appEvents.off("deadline:changed", this, this.#update);
+		this.appEvents.off("page:changed", this, this.#onPageChanged);
+		super.willDestroy(...arguments);
+	}
 
-    #init(topic) {
-        this.#topic = topic;
-        if (
-            this.#topic.deadline_timestamp === null ||
-            this.#topic.deadline_timestamp === ''
-        ) {
-            this.#timestamp = null;
-            this.#remainingDays = null;
-        } else {
-            this.#timestamp = parseInt(this.#topic.deadline_timestamp);
-            this.#remainingDays = getDeadlineRemainingDays(this.#timestamp);
-        }
-    }
+	#init(topic) {
+		this.#topic = topic;
+		if (
+			this.#topic.deadline_timestamp === null ||
+			this.#topic.deadline_timestamp === ""
+		) {
+			this.#timestamp = null;
+			this.#remainingDays = null;
+		} else {
+			this.#timestamp = parseInt(this.#topic.deadline_timestamp);
+			this.#remainingDays = getDeadlineRemainingDays(this.#timestamp);
+		}
+	}
 
-    async #update() {
-        const topic = await ajax(`/t/${this.#topic.id}.json`);
-        this.#init(topic);
-        this.updateDeadline();
-        this.args.outletArgs.model.deadline_timestamp =
-            this.deadlineTimestamp?.toString() ?? '';
-    }
+	async #update() {
+		const topic = await ajax(`/t/${this.#topic.id}.json`);
+		this.#init(topic);
+		this.updateDeadline();
+		this.args.outletArgs.model.deadline_timestamp =
+			this.deadlineTimestamp?.toString() ?? "";
+	}
 
-    #onPageChanged() {
-        this.#init(this.args?.outletArgs?.model);
-        this.updateDeadline();
-    }
+	#onPageChanged() {
+		this.#init(this.args?.outletArgs?.model);
+		this.updateDeadline();
+	}
 
-    #shouldRender() {
-        const {
-            deadline_enabled: deadlineEnabled,
-            deadline_allowed_on_categories: deadlineAllowedOnCategories,
-            deadline_display_on_closed_topic: deadlineDisplayOnClosedTopic,
-            deadline_display_on_solved_topic: deadlineDisplayOnSolvedTopic,
-        } = this.siteSettings;
-        const categoryId = this.#topic.category_id;
-        const deadlineAllowedCategories = getDeadlineAllowedCategories(
-            deadlineAllowedOnCategories,
-        );
-        const closedTopicDisplayDeadline =
-            !this.#topic.closed || deadlineDisplayOnClosedTopic;
-        const solvedTopicDisplayDeadline =
-            !this.#topic.has_accepted_answer || deadlineDisplayOnSolvedTopic;
+	#shouldRender() {
+		const {
+			deadline_enabled: deadlineEnabled,
+			deadline_allowed_on_categories: deadlineAllowedOnCategories,
+			deadline_display_on_closed_topic: deadlineDisplayOnClosedTopic,
+			deadline_display_on_solved_topic: deadlineDisplayOnSolvedTopic,
+		} = this.siteSettings;
+		const categoryId = this.#topic.category_id;
+		const deadlineAllowedCategories = getDeadlineAllowedCategories(
+			deadlineAllowedOnCategories,
+		);
+		const closedTopicDisplayDeadline =
+			!this.#topic.closed || deadlineDisplayOnClosedTopic;
+		const solvedTopicDisplayDeadline =
+			!this.#topic.has_accepted_answer || deadlineDisplayOnSolvedTopic;
 
-        return (
-            deadlineEnabled &&
-            closedTopicDisplayDeadline &&
-            solvedTopicDisplayDeadline &&
-            (deadlineAllowedCategories?.includes(categoryId) ?? true)
-        );
-    }
+		return (
+			deadlineEnabled &&
+			closedTopicDisplayDeadline &&
+			solvedTopicDisplayDeadline &&
+			(deadlineAllowedCategories?.includes(categoryId) ?? true)
+		);
+	}
 }
