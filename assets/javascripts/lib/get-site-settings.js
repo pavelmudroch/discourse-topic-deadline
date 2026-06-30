39b95925e9ee5ba3fe900af0fe91c6805e7175e1
pavelmudroch <xmudrochpavel@gmail.com>
2026-06-30 16:09:18 +0200

    fix: deprecated site-settings:main lookup

--- assets/javascripts/lib/get-site-settings.js
+++ assets/javascripts/lib/get-site-settings.js
@@ -1,15 +1,15 @@
-import { getDeadlineAllowedCategories } from './get-deadline-allowed-categories';
+import { getDeadlineAllowedCategories } from "./get-deadline-allowed-categories";
 
 export function getSiteSettings(api) {
-    const settings = api.container.lookup('site-settings:main');
-    return {
-        deadlineEnabled: settings.deadline_enabled,
-        deadlineSoonDays: settings.deadline_soon_days,
-        deadlineSoonDaysThreshold: settings.deadline_soon_days_threshold,
-        deadlineAllowedCategories: getDeadlineAllowedCategories(
-            settings.deadline_allowed_on_categories,
-        ),
-        deadlineDisplayOnClosedTopic: settings.deadline_display_on_closed_topic,
-        deadlineDisplayOnSolvedTopic: settings.deadline_display_on_solved_topic,
-    };
+	const settings = api.container.lookup("service:site-settings");
+	return {
+		deadlineEnabled: settings.deadline_enabled,
+		deadlineSoonDays: settings.deadline_soon_days,
+		deadlineSoonDaysThreshold: settings.deadline_soon_days_threshold,
+		deadlineAllowedCategories: getDeadlineAllowedCategories(
+			settings.deadline_allowed_on_categories,
+		),
+		deadlineDisplayOnClosedTopic: settings.deadline_display_on_closed_topic,
+		deadlineDisplayOnSolvedTopic: settings.deadline_display_on_solved_topic,
+	};
 }
